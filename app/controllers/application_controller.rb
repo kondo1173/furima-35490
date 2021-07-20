97faa27c1edcca40e8810e7f7e612afe943ac775
kondo1173 <migumeudonko@icloud.com>
2021-07-20 16:19:29 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :kanji_firstname, :kanji_lastname, :katakana_firstname, :katakana_lastname, :birthday])
+  end
+  
 end
