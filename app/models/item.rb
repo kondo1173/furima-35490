f3f1d1a8c281ba98ff0fa5522951ddc4915f563f
kondo1173 <migumeudonko@icloud.com>
2021-07-31 15:46:44 +0900

    モデルの単体テストコードを書く

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,24 +9,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  
-  
-  
   with_options presence: true do
-    validates :name
-    validates :info
+    validates :name, length:{ maximum: 40 }
+    validates :info, length:{ maximum: 1000 }
     validates :category_id,                numericality:{ other_than: 1 , message: "can't be blank"}
     validates :sales_status_id,            numericality:{ other_than: 1 , message: "can't be blank"} 
     validates :shopping_fee_status_id,     numericality:{ other_than: 1 , message: "can't be blank"}   
     validates :prefecture_id,              numericality:{ other_than: 1 , message: "can't be blank"} 
     validates :scheduled_delivery_id,      numericality:{ other_than: 1 , message: "can't be blank"} 
-    validates :price,                      numericality:{ greater_than: 300, less_than: 9999999, message: "Price is out of setting range"}
+    validates :price,                      numericality:{ greater_than: 300, less_than: 9999999, message: "is out of setting range"}
     validates :image
   end
 
-  PRICE_REGEX = /\A[0-9]+\z/.freeze
-  validates_format_of :price, with: PRICE_REGEX, message: 'Price is invalid. Input half-width characters'
-
+  validates :price, numericality: { only_integer: true, message: 'is invalid. Input half-width characters' }
 
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,93 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#create' do
+    before do
+    @item = FactoryBot.build(:item)
+    end
+    
+    describe '商品の出品' do
+      context "商品が保存できる場合" do
+        it "画像と商品名と商品説明、商品の詳細（カテゴリー、商品の状態、配送料の負担、発送元の地域、発送までの日数、価格）があれば投稿できる" do
+          expect(@item).to be_valid
+        end
+      end
+ 
+      context "商品が保存できない場合" do
+        it "画像がなければ、出品できない" do
+          @item.image = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Image can't be blank")
+        end
+
+        it "商品のカテゴリー選択がなければ、出品できない" do
+          @item.category_id = "1"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Category can't be blank")
+        end
+
+        it "商品の状態の説明がなければ、出品できない" do
+          @item.sales_status_id = "1"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Sales status can't be blank")
+        end
+
+        it "配送料の負担説明がなければ、出品できない" do
+          @item.shopping_fee_status_id = "1"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Shopping fee status can't be blank")
+        end
+
+        it "発送元地域の説明がなければ、出品できない" do
+          @item.prefecture_id = "1"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+        end
+
+
+        it "発送までの日数説明がなければ、出品できない" do
+          @item.scheduled_delivery_id = "1"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
+        end
+
+
+        it "価格が空では、出品できない" do
+          @item.price = ""
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price can't be blank")
+        end
+
+        it "価格が300円より少ない場合は、出品できない" do
+          @item.price = "200"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price is out of setting range")
+        end
+
+        it "価格が9,999,999円より多い場合は、出品できない" do
+          @item.price = "10000000"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price is out of setting range")
+        end
+
+        it "価格が全角では出品できない" do
+          @item.price = "１０００"
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters")
+        end
+
+
+        it "ユーザーが紐づいていないと出品できない" do
+          @item.user = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("User must exist")
+        end
+
+      end
+
+    end
+
+
+  end
 end
